253bb44bd53e2eb15ec4de1abe5813145ad2ab98
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-08-06 13:06:52 +0200

    chore(api): disabling playground

--- api/src/index.ts
+++ api/src/index.ts
@@ -145,7 +145,7 @@ async function asyncMain() {
     oauth2Providers: [],
     logger,
     urlAdapter: new EasyvoteURLAdapter({websiteURL}),
-    playground: true,
+    playground: false,
     introspection: true,
     tracing: true
   })
