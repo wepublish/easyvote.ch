0cf27c3ddc2db880e1f0d1800bf4532fde780f6c
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-10-26 11:36:26 +0200

    feat(app): updates to wepublish v2.10.0

--- api/src/index.ts
+++ api/src/index.ts
@@ -93,6 +93,7 @@ async function asyncMain() {
         input: {
           email: 'admin@easyvote.ch',
           name: 'Admin',
+          emailVerifiedAt: null,
           roleIDs: [adminUserRoleId],
           active: true,
           properties: []
